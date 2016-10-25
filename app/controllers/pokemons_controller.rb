5cb05992fb97020115da7951cc8ac7ad83e082d6
Roberto Romo <rromo@berkeley.edu>
2016-10-25 00:30:05 -0700

    catch 'em all

---
+++ app/controllers/pokemons_controller.rb
@@ -0,0 +1,19 @@
+class PokemonsController < ApplicationController
+  def capture
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.trainer = current_trainer
+    @pokemon.save
+    redirect_to root_path
+  end
+
+  def damage
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.health -= 10
+    @pokemon.save
+    if @pokemon.health <= 0
+      @pokemon.destroy
+    end
+    redirect_to trainer_path(id: @pokemon.trainer)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+
+  patch 'capture', to: 'pokemons#capture'
+  patch 'damage', to: 'pokemons#damage'
+
+
+
+
 end
